5d149e2d03c771d9e49df3222e985fb4345d00bc
srdeange <srdeange@ncsu.edu>
2022-02-10 17:35:12 -0500

    Added Skeleton for neosource to git

---
+++ neosource.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    print("parse input")
+    print("Run Dependency check tool")
+    print("Run Pendo Processes with json file")
+    print("FORCE open neo4J")
+    print("All while it print logs to user so they know shtuff happening")
+    print("Step 3: Profit")
